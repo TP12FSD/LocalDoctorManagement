91de11f9c2b80033c9db118f7699ffe8fc2582af
NISTELROOY LIM SHAO ZHE <2104440I@student.tp.edu.sg>
2022-12-07 22:21:18 +0800

    Added Edit, Create and Delete function

---
+++ LocalDoctorManagement/Client/Static/Endpoints.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalDoctorManagement.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string PatientsEndpoint = $"{Prefix}/patients";
+    }
+}
--- LocalDoctorManagement/Shared/Domain/Patient.cs
+++ LocalDoctorManagement/Shared/Domain/Patient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,14 @@ namespace LocalDoctorManagement.Shared.Domain
 {
     public class Patient : BaseDomainModel
     {
+        [Required]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Name does not meet length requirements")]
         public string Name { get; set; }
+        [Required]
         public string PillTime { get; set; }
+        [Required]
         public string WaterTime { get; set; }
+        [Required]
         public string FoodRestrictions { get; set; }
     }
 }
